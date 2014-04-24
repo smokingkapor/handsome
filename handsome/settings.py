4b27238abcbe8a0177e0c6e9637bbfaac6b71b18
Kapor <smokingkapor@gmail.com>
2014-04-24 12:10:01 +0800

    Highlight selected top menu

--- handsome/settings.py
+++ handsome/settings.py
@@ -92,6 +92,17 @@ USE_L10N = True
 USE_TZ = False
 
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+)
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
