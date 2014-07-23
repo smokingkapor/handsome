a56003d84918c3e08aaa66ca7b2a0464f7960631
Kapor <smokingkapor@gmail.com>
2014-07-23 12:08:10 +0800

    Add admin file

--- designs/admin.py
+++ designs/admin.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from .models import Design, DesignPhoto
+from .models import Design, DesignPhoto, DesignClothing
 
 
 admin.site.register(Design)
 admin.site.register(DesignPhoto)
+admin.site.register(DesignClothing)
