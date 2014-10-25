f93053115c4c110021e828b13516e7a3dd7c57af
Kapor <smokingkapor@gmail.com>
2014-10-25 16:37:30 +0800

    Added design photos view

--- designs/urls.py
+++ designs/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 
 from .views import(
     CreateDesignView, UploadView, DesignDetailView, AcceptDesignView,
-    RemoveDesignView
+    RemoveDesignView, DesignPhotosView
 )
 
 
@@ -14,5 +14,6 @@ urlpatterns = patterns(
     url(r'^(?P<code>\d+)/$', DesignDetailView.as_view(), name='detail'),
     url(r'^(?P<code>\d+)/remove/$', RemoveDesignView.as_view(), name='remove'),
     url(r'^(?P<code>\d+)/select/$', AcceptDesignView.as_view(), name='accept'),
+    url(r'^(?P<code>\d+)/photos/$', DesignPhotosView.as_view(), name='photos'),
     # url(r'^(?P<code>\d+)/reject/$', RejectDesignView.as_view(), name='reject'),
 )
